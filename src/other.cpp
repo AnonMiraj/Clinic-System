921097a8c61660e2e82ab4afa019274e0710bcc1
Abdulra7man10 <abdo144418@gmail.com>
2023-05-27 22:11:05 +0300

    New class for functions to use in whole program

---
+++ src/other.cpp
@@ -0,0 +1,36 @@
+#include "other.h"
+
+bool other::IsValid(string msg)
+{
+  bool IsNotValid = false;
+  string num;
+  do
+  {
+    IsNotValid = false;
+
+    try
+    {
+      cout << msg;
+      cin >> num;
+
+      for (int i = 0; i < num.size(); i++)
+        if (!isdigit(num[i]))
+        {
+          IsNotValid = true;
+          break;
+        }
+
+      if (IsNotValid)
+        throw "Error";
+
+      return true;
+    }
+
+    catch (const char *str)
+    {
+      cerr << "Please, don't write letter. type only from these choices :)\n";
+    }
+  } while (IsNotValid);
+
+  return true;
+}
