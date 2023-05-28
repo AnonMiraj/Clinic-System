55bfa39c3e522013152f76157c32d29889523df6
mohamed-1357 <112498629+mohamed-1357@users.noreply.github.com>
2023-05-28 05:08:59 +0300

    Update
    I think I losing my Mind

--- ClinicSystem_V2/OrderItem.h
+++ ClinicSystem_V2/OrderItem.h
@@ -1,5 +1,30 @@
 #pragma once
-class OrderItem
+#include "Medcin.h"
+#include "Stock.h"
+class orderItem
 {
-};
+private:
+	double sale_price;
+	int qunatityOrderItem;
+	Stock* stk;
+	int idOrderItem;
 
+public:
+	orderItem();
+	orderItem(Stock&);
+
+	void setOrderItem(int id, Stock& s);
+	void UpdateQuantity(int newQuantity);
+	int getIdOrderItem();
+	void setSalePrice(double);
+	double getSalePrice();
+	void setQuantityOfOrderItem(int);
+	double getQuantityOfOrderItem();
+	void EditOrderItem();
+	orderItem operator++();
+	orderItem operator--();
+	orderItem operator+=(int quantity);
+	orderItem operator-=(int quantity);
+	friend ostream& operator<<(ostream&, orderItem&);
+
+};
